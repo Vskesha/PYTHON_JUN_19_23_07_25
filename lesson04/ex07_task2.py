c697659dc00f52a723494a3a3d8e4f479c0d6910
Vasyl Boliukh <vskesha@gmail.com>
2025-08-27 13:00:07 +0300

    added code examples to lesson04

---
+++ lesson04/ex07_task2.py
@@ -0,0 +1,15 @@
+# ------------------------------------------
+# Завдання 2
+# Створіть п’ять змінних для відображення різних фруктів.
+
+fruit1 = "apple"
+fruit2 = "banana"
+fruit3 = "pear"
+fruit4 = "grape"
+fruit5 = "orange"
+
+print(fruit1)
+print(fruit2)
+print(fruit3)
+print(fruit4)
+print(fruit5)
