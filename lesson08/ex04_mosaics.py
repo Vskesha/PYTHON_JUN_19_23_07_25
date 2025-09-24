cb8e305145e396a2eee119ea455140fbd251f4c5
Vasyl Boliukh <vskesha@gmail.com>
2025-09-24 18:27:21 +0300

    added code examples to lesson08

---
+++ lesson08/ex04_mosaics.py
@@ -0,0 +1,22 @@
+import turtle
+import random
+
+from settings import size, colors
+
+
+screen = turtle.Screen()
+t = turtle.Turtle()
+t.speed(0)
+t.pensize(3)
+
+for x in range(-5 * size, size * 5, size):
+    t.color(random.choice(colors))
+    for y in range(-5 * size, size * 5, size):
+        t.penup()
+        t.goto(x, y)
+        t.pendown()
+        for _ in range(4):
+            t.forward(size - 3)
+            t.right(90)
+
+screen.mainloop()
