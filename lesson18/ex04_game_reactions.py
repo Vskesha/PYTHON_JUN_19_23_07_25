9c163ca1b55d6fb0af361c62d37d3eeca8e02d1b
Vasyl Boliukh <vskesha@gmail.com>
2025-12-03 15:21:23 +0200

    Add code examples to lesson18

---
+++ lesson18/ex04_game_reactions.py
@@ -0,0 +1,37 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Ігрові реакції")
+
+square = pygame.Rect(350, 250, 100, 100)
+color = (0, 128, 255)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if square.collidepoint(event.pos):
+                color = (255, 0, 0)
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            color = (0, 128, 255)
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                square.x -= 50
+            elif event.key == pygame.K_RIGHT:
+                square.x += 50
+            elif event.key == pygame.K_UP:
+                square.y -= 50
+            elif event.key == pygame.K_DOWN:
+                square.y += 50
+
+    screen.fill((255, 255, 255))
+    pygame.draw.rect(screen, color, square)
+    pygame.display.flip()
+
+pygame.quit()
---
+++ lesson18/ex06_conditional_reactions2.py
@@ -0,0 +1,46 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Взаємодія між об'єктами")
+
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+player = pygame.Rect(100, 100, 100, 100)
+target = pygame.Rect(400, 300, 100, 100)
+
+speed = 1
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player.x -= speed
+    if keys[pygame.K_RIGHT]:
+        player.x += speed
+    if keys[pygame.K_UP]:
+        player.y -= speed
+    if keys[pygame.K_DOWN]:
+        player.y += speed
+
+    if player.colliderect(target):
+        target_color = RED
+    else:
+        target_color = BLUE
+
+    screen.fill(WHITE)
+
+    pygame.draw.rect(screen, target_color, target)
+    pygame.draw.rect(screen, GREEN, player)
+
+    pygame.display.flip()
+    pygame.time.delay(5)
+
+pygame.quit()
