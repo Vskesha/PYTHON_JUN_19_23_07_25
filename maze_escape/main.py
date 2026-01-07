0755b834d990ff61cda551b09588e4cbb8abd89d
Vasyl Boliukh <vskesha@gmail.com>
2026-01-07 20:58:03 +0200

    Add maze walls, key and door

---
+++ maze_escape/main.py
@@ -0,0 +1,77 @@
+import pygame
+from pathlib import Path
+import random
+
+pygame.init()
+
+# Створення вікна гри
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Втеча з лабіринту')
+
+# Фоновий колір
+background_color = (0, 0, 0)  # Чорний колір фону
+
+cell_size = 40
+
+assets_path = Path(__file__).parent.joinpath("assets")
+wall_img = pygame.image.load(assets_path.joinpath("wall2.jpg"))
+wall_img = pygame.transform.scale(wall_img, (cell_size, cell_size))
+
+key_img = pygame.image.load(assets_path.joinpath("key.png"))
+key_img = pygame.transform.scale(key_img, (cell_size, cell_size))
+
+door_img = pygame.image.load(assets_path.joinpath("door.png"))
+door_img = pygame.transform.scale(door_img, (cell_size, cell_size))
+
+
+maze = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+height = len(maze)
+width = len(maze[0])
+
+free_cells = []
+for y in range(height):
+    for x in range(width):
+        if maze[y][x] == 0:
+            free_cells.append((x, y))
+
+key_position = random.choice(free_cells[1:-1])
+door_position = free_cells[-1]
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Заповнюємо екран фоном
+    screen.fill(background_color)
+
+    for y in range(height):
+        for x in range(width):
+            if maze[y][x] == 1:
+                screen.blit(wall_img, (x * cell_size, y * cell_size))
+    
+    screen.blit(key_img, (key_position[0] * cell_size, key_position[1] * cell_size))
+    screen.blit(door_img, (door_position[0] * cell_size, door_position[1] * cell_size))
+
+    # Оновлюємо екран
+    pygame.display.flip()
+
+pygame.quit()
